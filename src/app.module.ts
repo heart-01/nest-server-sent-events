f6e1f1e16526d3a686f669046bce8e4e936beed7
siwat jermwatthana <siwat.j@hiveground.com>
2023-12-12 12:31:29 +0700

    [Feat]: Add ServeStaticModule to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { EventsModule } from './services/events/events.module';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [EventsModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+    EventsModule,
+  ],
   controllers: [],
   providers: [],
 })
